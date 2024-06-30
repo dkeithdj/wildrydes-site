855697940d0b3b5e0ea4f45063adb0c3454e0504
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-06-30 22:16:23 +0800

    feat: add cognito settings

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "ap-southeast-1_CZoi5R3GT", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "369hg6h9j72f2gqil5bj85j36u", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "ap-southeast-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
