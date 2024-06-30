520ea37685d6aa9139d17416687a35fc886f2100
Denrei Keith <42316655+dkeithdj@users.noreply.github.com>
2024-06-30 23:54:56 +0800

    feat: add invokeURL

--- js/config.js
+++ js/config.js
@@ -5,6 +5,7 @@ window._config = {
     region: "ap-southeast-1", // e.g. us-east-2
   },
   api: {
-    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    invokeUrl:
+      "https://x6eykiuwsl.execute-api.ap-southeast-1.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
   },
 };
